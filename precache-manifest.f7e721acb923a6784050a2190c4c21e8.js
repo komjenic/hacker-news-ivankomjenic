679d2743a6433260dc85d5460c1cd5a72998de2b
Ivan Komjenic <komjenic@gmail.com>
2019-09-26 17:02:13 +0200

    Updates

---
+++ precache-manifest.f7e721acb923a6784050a2190c4c21e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b27437b4fce07c064733fb9f0434ff4",
+    "url": "/hacker-news-ivankomjenic/index.html"
+  },
+  {
+    "revision": "e99bf4ed26d5d7c43a7c",
+    "url": "/hacker-news-ivankomjenic/static/js/2.a2de260f.chunk.js"
+  },
+  {
+    "revision": "6323734cfe2aa6159faa",
+    "url": "/hacker-news-ivankomjenic/static/js/main.3b85a395.chunk.js"
+  },
+  {
+    "revision": "4ad7b8c4320f18df3bb1",
+    "url": "/hacker-news-ivankomjenic/static/js/runtime-main.ca1cd8a9.js"
+  }
+]);
